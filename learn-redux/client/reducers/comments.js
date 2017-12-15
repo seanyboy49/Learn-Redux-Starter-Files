990cb5d1417eb0563edaaa1689aa47cf86aa6540
Sean Lee <seanboramlee@gmail.com>
2017-12-15 13:57:25 -0800

    modify posts reducer to return new state

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,6 +1,4 @@
 function comments (state = [], action) {// set state to be an empty array
-  console.log("This is comments.");  
-  console.log(state, action);
   return state;
 } 
 
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,8 +4,18 @@
 // 2. A copy of current state
 
 function posts (state = [], action) {// set state to be an empty array
-  console.log("This is posts.");
-  console.log(state, action);
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log("Incrementing likes!")
+      const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i],likes: state[i].likes + 1 },
+        ...state.slice(i+1) // after the one we are updating
+      ]
+    default:
+      return state;
+  }
   return state;
 } 
 
