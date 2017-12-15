874f18f88af64bd4f3a81de0c75b1e9e02586140
Sean Lee <seanboramlee@gmail.com>
2017-12-14 16:58:37 -0800

    create store

---
+++ learn-redux/client/store.js
@@ -0,0 +1,23 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+// Import root reducer 
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// Create object for the default data 
+// ES6 shortcut if property key = value
+const defaultState = {
+  posts,
+  comments
+}
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store) // browserHistory keeps track of navigating
+
+export default store; // You can have one default export per file
+
